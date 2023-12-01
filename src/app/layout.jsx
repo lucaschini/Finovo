8c25961b9ec28032534cca727fd65b48bbb3dbb0
Lucas Chini <lucaschinimendonca@protonmail.com>
2023-12-01 12:59:05 -0300

    font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,9 +2,17 @@ import Image from "next/image";
 import "./globals.css";
 import logo from "../../public/logo.svg";
 
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={poppins.className}>
       <body>{children}</body>
     </html>
   );
