2029e221e9df53eb6fd3ff4f65c34ab08129a541
Lucas Chini <lucaschinimendonca@protonmail.com>
2023-12-02 00:08:29 -0300

    Front-end terminado

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -6,11 +6,12 @@ import Nav from "@/components/Nav";
 
 export default function DashBoardLayout({ children }) {
   return (
-    <div className="h-screen w-screen text-white flex bg-[#050112]">
-      <div className="h-screen w-1/4 bg-mainPurple rounded-tr-[25px] rounded-br-[25px] shadow-md flex flex-col items-center justify-center">
-      <Image src={logo} className="w-[80px] absolute top-28"/>
+    <div className="h-screen w-screen text-white flex bg-darkBg">
+      <div className="h-screen w-[400px] bg-mainPurple rounded-tr-[25px] rounded-br-[25px] shadow-md flex flex-col items-center justify-center">
+      <Image src={logo} className="w-[130px] absolute top-28"/>
       <Nav />
-      <Image src={npp} className="w-[58px] rounded-full absolute left-2 bottom-2"/>
+      <Link href='/perfil' > <Image src={npp} className="w-[58px] rounded-full absolute left-2 bottom-2"/> </Link>
+     
       </div>
       <div className="w-full flex justify-center items-center text-white">{children}</div>
     </div>
--- src/app/(login)/repass1/page.jsx
+++ src/app/(login)/repass1/page.jsx
@@ -4,11 +4,11 @@ import logo from "../../../../public/logo.svg";
 import Seta from "../../../../public/iconContinuar.png";
 
 export const metadata = {
-  title: "Login",
+  title: "Redefina sua senha",
   description: "Entre em sua conta",
 };
 
-export default function Login() {
+export default function Repass1() {
   return (
     <div className='text-white w-screen h-screen flex justify-center items-center flex-col'>
       <div className="h-[74%] w-[70vw] rounded-[15px] bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50 flex">
@@ -16,19 +16,17 @@ export default function Login() {
           <Image src={logo}  className="max-w-[150px]"/>
         </div>
         <div className="h-full w-full flex flex-col pt-20 gap-20 pl-20 items-start">
-          <h1 className="text-2xl font-medium ">Acesse sua conta</h1>
-          <form className="flex flex-col gap-3 w-1/2">
-            <label htmlFor="email">Email</label>
+          <h1 className="text-2xl font-medium ">Redefina sua senha</h1>
+          <form className="flex flex-col gap-6 w-1/2">
+            <label htmlFor="email">Nova senha</label>
             <input type="text" id="email" className="bg-transparent p-2 border-b-2 focus:outline-none"/>
-            <label htmlFor="senha">Senha</label>
+            <label htmlFor="senha">Confirmar Nova Senha</label>
             <input type="password" id="senha" className="bg-transparent p-2 border-b-2 focus:outline-none"/>
-            <Link className="text-xs hover:text-violet-500" href="/repass">Esqueci a senha</Link>
             <Link href='/home' className=" flex justify-center items-center gap-8 text-xl px-5 py-5 mt-2 bg-[#7258B9] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-70 shadow-2xl rounded-full">
-              Continuar
+              Redefinir senha
               <Image src={Seta} width={20} alt='seta'/>
             </Link>
           </form>
-          <Link href="/create" className=" hover:text-violet-500">Não tenho uma conta</Link>
         </div>
       </div>
     </div>
